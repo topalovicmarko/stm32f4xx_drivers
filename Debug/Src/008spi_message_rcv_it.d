a736ae49e297634b6559c487395ca3d282852c84
topalovicmarko <marko.topalovic99@gmail.com>
2025-08-19 13:16:56 +0200

    I2C Driver API

--- Debug/Src/008spi_message_rcv_it.d
+++ Debug/Src/008spi_message_rcv_it.d
@@ -2,8 +2,10 @@ Src/008spi_message_rcv_it.o: ../Src/008spi_message_rcv_it.c \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h \
- D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h
+ D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h \
+ D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_i2c_driver.h
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h:
+D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_i2c_driver.h:
--- Debug/drivers/Src/stm32f401xx_gpio_driver.d
+++ Debug/drivers/Src/stm32f401xx_gpio_driver.d
@@ -3,8 +3,10 @@ drivers/Src/stm32f401xx_gpio_driver.o: \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h \
  D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h \
- D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h
+ D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h \
+ D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_i2c_driver.h
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_gpio_driver.h:
 D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_spi_driver.h:
+D:/Workspace/MasteringMCU-master/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f401xx_i2c_driver.h:
